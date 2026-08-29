17c2ba4c562de5f4cef48e49c96f6821cd1239a7
shayi <grshayiny2301@gmail.com>
2026-08-29 11:55:51 +0530

    first commit

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,9 @@
+using Backend.Data;
+using Backend.Repositories.Company.Implementations;
+using Backend.Repositories.Company.Interfaces;
+using Backend.Services.Company.Implementations;
+using Backend.Services.Company.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend
 {
@@ -7,10 +13,17 @@ namespace Backend
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(
+                 builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+
+            builder.Services.AddScoped<ICompanyService, CompanyService>();
+
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
